95e2b0a05506c795dd0b41a7ab51553fa4f16f92
regedit15 <001regedit15@gmail.com>
2015-09-05 10:35:16 -0300

    se mejora el calculo de precio

--- smartPrice/www/js/index.js
+++ smartPrice/www/js/index.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
 
+    var precioVacio = "$ 0";
 
     $("#btn_agregar").click(function() {
-        var $row = $("<tr><td><h4 class='titulo'>Precio</h4><input class='form-control' id='exampleInputEmail1'></td><td><h4 class='titulo'>Cantidad</h4><input class='form-control' id='exampleInputEmail1'></td><td><h4 class='precio'>Resultado</h4><h4 class='precio'>$55.50</h4></td></tr>");
+        var fila =
+            "<tr>" +
+            "   <td>" +
+            "       <h4 class='titulo'>Precio</h4>" +
+            "       <input class='form-control' id='precio'>" +
+            "   </td>" +
+            "   <td>" +
+            "       <h4 class='titulo'>Cantidad</h4>" +
+            "       <input class='form-control' id='cantidad'>" +
+            "   </td>" +
+            "   <td>" +
+            "       <h4 class='precio'>Resultado</h4>" +
+            "       <div class='alert alert-success'>" +
+            "           <h4 id='resultado' class='precio'>" + precioVacio + "</h4>" +
+            "       </div>" +
+            "   </td>" +
+            "</tr>";
 
-        $('table').append($row);
+        $('table').append(fila);
     });
 
-
+    // para cargar la fila al cargar la pagina
+    $('#btn_agregar').click();
 
     $("input").keyup(function(event) {
         var precio = $("#precio").val();
@@ -16,14 +34,10 @@ $(document).ready(function() {
         if (precio != '' && cantidad != '') {
             $("#resultado").text('$' + ((1000 * precio) / cantidad).toFixed(2));
         } else {
-            $("#resultado").text('');
+            $("#resultado").text(precioVacio);
         }
-
-
-
-
-
     });
+
 });
 
 
