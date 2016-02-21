586c6b2ca0e808fcba04b03bff38be8d84141065
regedit15 <001regedit15@gmail.com>
2016-02-21 16:08:38 -0300

    se realizan mejoras de codigo

--- smartPrice/www/js/controllers/smartPriceController.js
+++ smartPrice/www/js/controllers/smartPriceController.js
@@ -1,29 +1,27 @@
 var app = angular.module('smartPrice', [ 'ngRoute', 'ngMessages' ]);
 
-app.controller('smartPriceController',
-		function($scope) {
+app.controller('smartPriceController', function($scope) {
 
-			$scope.hola = 'aaaaa3333';
+	$scope.hola = 'aaaaa3333';
 
-			$scope.calculos = [];
+	$scope.calculos = [];
 
-			$scope.calculos.push(new Calculo(null, null, null));
+	$scope.calculos.push(new Calculo(null, null, null));
 
-			$scope.calcular = function(x) {
+	$scope.calcular = function(x) {
 
-				var result = ((Number(x.precio) * 100) / Number(x.cantidad))
-						.toFixed(2);
+		var result = ((Number(x.precio) * 100) / Number(x.cantidad)).toFixed(2);
 
-				if (isNaN(result) || !isFinite(result)) {
-					result = '';
-				}
+		if (isNaN(result) || !isFinite(result)) {
+			result = '';
+		}
 
-				x.resultado = result;
-			}
+		x.resultado = result;
+	}
 
-			$scope.agregar = function() {
+	$scope.agregar = function() {
 
-				$scope.calculos.push(new Calculo(null, null, null));
-			}
+		$scope.calculos.push(new Calculo(null, null, null));
+	}
 
-		});
+});
